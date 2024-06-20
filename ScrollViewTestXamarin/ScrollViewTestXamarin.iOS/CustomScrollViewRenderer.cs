ad16f1c83d6972fb4971390b7b585e3184574ae6
Michael Blades <michaelblades12@gmail.com>
2024-06-20 09:58:44 -0500

    Add project files.

---
+++ ScrollViewTestXamarin/ScrollViewTestXamarin.iOS/CustomScrollViewRenderer.cs
@@ -0,0 +1,40 @@
+﻿using ScrollViewTestXamarin;
+using ScrollViewTestXamarin.iOS;
+using System;
+using System.Linq;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomScrollView), typeof(CustomScrollViewRenderer))]
+namespace ScrollViewTestXamarin.iOS
+{
+	public class CustomScrollViewRenderer : ScrollViewRenderer
+	{
+        public CustomScrollViewRenderer()
+        {
+            MaximumZoomScale = 5;
+            MinimumZoomScale = 1;
+        }
+
+		protected override void OnElementChanged(VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement != null)
+			{
+				ViewForZoomingInScrollView -= GetViewForZooming;
+			}
+
+			if (e.NewElement != null)
+			{
+				ViewForZoomingInScrollView += GetViewForZooming;
+			}
+		}
+
+		private UIView GetViewForZooming(UIScrollView sv)
+		{
+			return Subviews.FirstOrDefault();
+		}
+	}
+}
